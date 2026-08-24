f55e93d9a9a11f2182d6a7b32a1dba2525031a9e
Hena Ahmed <59346862+hahmed17@users.noreply.github.com>
2026-08-24 11:57:29 -0700

    Update install_packages.sh

--- install_packages.sh
+++ install_packages.sh
@@ -1,28 +1,24 @@
 #!/bin/bash
+
 {
     set -euo pipefail
-
+    
     echo "PWD: $(pwd)"
-
+    echo "Python: $(python -c 'import sys; print(sys.executable)')"
+    
+    # Ensure conda commands work in non-interactive shells (safe even if already works)
     source "$(conda info --base)/etc/profile.d/conda.sh"
-
-    ENV_NAME="gdal_env"
-
-    # Create a fresh env with gdal 
-    conda create -n "$ENV_NAME" -c conda-forge python=3.11 gdal -y
-
-    conda activate "$ENV_NAME"
-
+    # conda update -n base -c conda-forge conda
+    conda activate base
+    
     echo "After conda activate:"
     which python
-    python -V
-
-    # Register this env as a Jupyter kernel
-    python -m pip install ipykernel
-    python -m ipykernel install --user --name "$ENV_NAME" --display-name "Python ($ENV_NAME)"
-
-    # Install the rest of the requirements
-    python -m pip install --upgrade pip
-    python -m pip install -r requirements.txt
-
+    /opt/conda/bin/python -V
+    /opt/conda/bin/python -m pip -V
+    
+    conda install -c conda-forge -y gdal gcc gxx geos
+    
+    /opt/conda/bin/python -m pip install --upgrade pip
+    /opt/conda/bin/python -m pip install -r requirements.txt
+    
 } >install.log 2>&1
